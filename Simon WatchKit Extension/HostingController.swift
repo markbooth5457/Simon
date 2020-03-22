d7162adac7e512b44d7a545744498bf321d5d17d
Mark Booth <mark@markandpol.com>
2020-03-22 16:32:03 +0000

    Initial Commit

---
+++ Simon WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Simon WatchKit Extension
+//
+//  Created by Mark Booth on 22/03/2020.
+//  Copyright © 2020 Mark Booth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
